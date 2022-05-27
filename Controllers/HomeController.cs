2a3fe8b7adfa9226ad5eed7a9ac3ccd00d81504d
syed inshal <syedinshal4@hotmail.com>
2022-05-28 00:05:22 +0500

    Files Added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using IceCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IceCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ViewResult SubHome() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult SubHome(Sub s) {
+
+            return View("SubHome", s);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,7 @@
+﻿namespace IceCore.Models
+{
+    public class Sub {
+        public static string? name { get; set; }
+        public static string? password { get; set; }
+    }
+}
